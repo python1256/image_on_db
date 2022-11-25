6b476d8f3dd3ea7341f1aaed33cc16d6a19fbad4
parth chaturvedi <parthbhai012@gmail.com>
2022-11-25 17:33:17 +0530

    first commit

---
+++ controller/user.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const path =require("path");
+const bodyparser=require("body-parser");
+const multer = require('multer');
+const { error } = require("console");
+const app=express();
+const router = express.Router();
+const Image_store=require("../model/image_model");
+
+
+
+const Storage=multer.diskStorage({
+    destination:function(req,file,cb){cb(null,'./uploads');},
+    filename:(req,file,cb)=>{
+       cb(null,file.originalname);
+    },
+});
+
+const upload=multer({
+    storage:Storage,
+}).single('testimage');
+
+router.get('/show_image',(req,res)=>{
+    Image_store.find({},(err,Items)=>{
+        if(err){
+            console.log(err);
+            res.status(400).send('an error');
+        }else{
+            res.send({Items:Items});
+        }
+    });
+})
+router.get("/show_by_id/:id",async(req,res)=>{
+    try{
+        const Id=req.params.id;
+        console.log(Id);
+        const foundimage=await Image_store.findOne({id:Id});
+        res.status(201).send(foundimage);
+    } catch(err){
+        res.status(400).send(err);
+    }
+})
+
+router.post("/upload_Image",(req,res)=>{
+    console.log("hello");
+    upload(req,res,(err)=>{
+        if(err){
+            console.log(err,"error");
+        }
+        else{
+            const newimage=new Image_store({
+                name:req.body.name,
+                image:{
+                    data:req.file.filename,
+                    contentType:'image/png'
+                } 
+
+            })
+            console.log(newimage);
+            newimage.save().then(()=>{
+                res.status(201).send("sucessfully upload");
+                //just need to change send to render and mention the home page
+            }).catch((err)=>{
+            res.status(400).send(err);
+            })
+        }
+    })
+});
+module.exports=router;
---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cors=require("cors");
+const app=express();
+const PORT = process.env.PORT || 8085;
+const host='0.0.0.0';
+const path = require("path");
+require("./db/conn");
+const exphbs = require("express-handlebars");
+const bodyparser =require("body-parser");
+
+
+
+app.use(cors(
+    {
+        origin:"http://localhost:3000",
+        credentials:true,
+        optionsSuccessStatus:200
+    }
+));
+app.use('/uploads', express.static('./uploads')); 
+
+//app.engine('handlebars',exphbs({extname:"hbs",defaultLayout:false,layoutDir:"views/"}));
+app.set('veiw engine','handlebars');
+
+//app.use('/image',express.static('upload/images'));
+
+//calling
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:false}));
+const routes = require("./controller/user");
+app.use('/', routes)
+
+app.listen(PORT,host,()=>
+{
+    console.log(`listening to port at ${PORT}`);
+})
+
