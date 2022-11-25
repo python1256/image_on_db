abb91180bb8d0ba7d3b7203e93f963fb12946ee9
parth chaturvedi <parthbhai012@gmail.com>
2022-11-25 18:10:39 +0530

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors=require("cors");
 const app=express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const host='0.0.0.0';
 const path = require("path");
 require("./db/conn");
